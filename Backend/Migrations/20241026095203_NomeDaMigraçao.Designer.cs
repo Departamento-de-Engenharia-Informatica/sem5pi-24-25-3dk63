f17593c30013991015d9f54e3731def6ca85e30b
1221288 <1221288@isep.ipp.pt>
2024-10-26 12:13:20 +0100

    resolve merge

--- Backend/Migrations/20241026095203_NomeDaMigraçao.Designer.cs
+++ Backend/Migrations/20241026095203_NomeDaMigraçao.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DDDNetCore.Migrations
 {
     [DbContext(typeof(DDDSample1DbContext))]
-    [Migration("20241026090807_NomeDaMigraçao")]
+    [Migration("20241026095203_NomeDaMigraçao")]
     partial class NomeDaMigraçao
     {
         /// <inheritdoc />
