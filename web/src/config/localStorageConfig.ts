cb65955a2271668b89b9e68fa09e1a3e05ffd09b
1221288 <1221288@isep.ipp.pt>
2024-11-03 18:30:41 +0000

    Remove uncessary files

--- web/src/config/localStorageConfig.ts
+++
@@ -1,3 +0,0 @@
-export const localStorageConfig = {
-  token: "@CliniTech:token",
-};
--- web/src/dto/IPaginationDTO.ts
+++
@@ -1,9 +0,0 @@
-export interface IPaginationDTO<T> {
-  meta?: {
-    page: number;
-    limit: number;
-    total: number;
-    totalPages: number;
-  };
-  data: T[];
-}
