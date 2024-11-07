489faaab63a24ba44cda9b30f9fea872398dcf1a
1221288 <1221288@isep.ipp.pt>
2024-11-07 13:44:32 +0000

    Backend : Adicionar 0.0.0.0 ao web builder no program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -17,7 +17,7 @@ namespace DDDSample1
                 {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://localhost:5000", "https://localhost:5001", "http://*:5000", "https://*:5001", "http://10.9.21.142:5000", "https://10.9.21.142:5001");
+                    webBuilder.UseUrls("http://localhost:5000", "https://localhost:5001", "http://*:5000", "https://*:5001", "http://10.9.21.142:5000", "https://10.9.21.142:5001", "https://0.0.0.0:5001", "http://0.0.0.0:5001");
                 });
         }
 }
