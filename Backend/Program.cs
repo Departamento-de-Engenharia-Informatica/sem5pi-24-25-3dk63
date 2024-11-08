07c963dc985955aae6dd0df328a37941a0ffe38f
1221288 <1221288@isep.ipp.pt>
2024-11-08 18:25:54 +0000

    Backend Module - alteracao para executar na maquina virtual linux

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -17,7 +17,15 @@ namespace DDDSample1
                 {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001");
+                    webBuilder.UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:11419");
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.ListenAnyIP(11419, listenOptions =>
+                        {
+                            listenOptions.UseHttps("/etc/ssl/certs/ca-certificates.crt", "/etc/ssl/private/server.key");
+                        });
+                    });
+
                 });
     }
 }
