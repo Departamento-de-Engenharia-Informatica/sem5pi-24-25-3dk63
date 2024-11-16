7458aacb204e1a2ec419953d64ec04255834baee
1221289 <1221289@isep.ipp.pt>
2024-11-16 10:55:42 +0000

    #58 - changed method to use the correct DTO

--- web/src/service/IService/IOperationRequestService.ts
+++ web/src/service/IService/IOperationRequestService.ts
@@ -1,10 +1,11 @@
 import { OperationRequest } from "@/model/OperationRequest";
 import { UpdateOperationRequestDTO } from "@/dto/UpdateOperationRequestDTO";
+import { CreatingOperationRequestDTO } from "@/dto/CreatingOperationRequestDTO";
 
 export interface IOperationRequestService {
     getOperationRequests(): Promise<OperationRequest[]>;
     searchOperationRequests(query: Record<string, string>): Promise<OperationRequest[]>;
-    createOperationRequest(operationRequest: UpdateOperationRequestDTO): Promise<void>;
+    createOperationRequest(operationRequest: CreatingOperationRequestDTO): Promise<void>;
     deleteOperationRequest(id: string): Promise<void>;
     editOperationRequest(operationRequest: UpdateOperationRequestDTO): Promise<void>;
     deactivateOperationRequest(id: string): Promise<void>;
