09e37c0100622b01de9d1ccec42463be9524bfe8
1221289 <1221289@isep.ipp.pt>
2024-11-17 06:52:05 +0000

    #52 - implemented AuthGuard to restrict access to unauthorized parts of the solution

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,7 +10,6 @@ import PatientAppointments from './pages/PatientAppointments';
 import PatientMedicalRecord from './pages/PatientMedicalRecord';
 import StaffPage from './pages/StaffMenu';
 import StaffOperationRequest from './pages/StaffOperationRequest';
-import ConfirmAccountDeletion from "./pages/ConfirmAccountDeletion";
 import SurgeryRoom from "./pages/SurgeryRoom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "inversify-react";
@@ -18,6 +17,10 @@ import { container } from "./inversify";
 import React, { useEffect, useState } from "react";
 import Floor3D2 from './pages/3DModel';
 import ThemeToggleButton from "./components/TheToggleButton";
+import ConfirmUpdatePage from "./pages/ConfirmAccountUpdatePatient";
+import ConfirmDeletionPage from "./pages/ConfirmAccountDeletionPatient";
+import ProtectedRoute from './components/ProtectedRoute';
+import NotAuthorizedPage from "./pages/NotAuthorized";
 
 function App() {
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
@@ -44,28 +47,105 @@ function App() {
     <Provider container={container} standalone>
       <Router>
         <div className="flex flex-col h-screen">
-     <header className="relative p-4 text-center flex-shrink-0 shadow-md bg-primary text-text">
-        <h1 className="text-lg font-semibold tracking-wide text-white">CliniTech Portal</h1>
-        <ThemeToggleButton theme={theme} toggleTheme={toggleTheme} />
-      </header>
+          <header className="relative p-4 text-center flex-shrink-0 shadow-md bg-primary text-text">
+            <h1 className="text-lg font-semibold tracking-wide text-white">CliniTech Portal</h1>
+            <ThemeToggleButton theme={theme} toggleTheme={toggleTheme} />
+          </header>
           <main className="flex flex-1 overflow-hidden bg-main-background text-main-text">
             <div className="flex-1 p-8 overflow-hidden">
-              {/* Conteúdo principal */}
+              {/* Main Content */}
               <Routes>
+                {/* Public Routes */}
                 <Route path="/" element={<LoginPage />} />
-                <Route path="/admin" element={<AdminPage />} />
-                <Route path="/staff" element={<StaffPage />} />
-                <Route path="/staff/operation-requests" element={<StaffOperationRequest setAlertMessage={setAlertMessage} />} />
-                <Route path="/admin/patient" element={<AdminPatient setAlertMessage={setAlertMessage} />} />
-                <Route path="/admin/staff" element={<AdminStaff setAlertMessage={setAlertMessage} />} />
-                <Route path="/staff/floor" element={<Floor3D2 />} />
-                <Route path="/patient" element={<PatientPage />} />
                 <Route path="/self-register" element={<SelfRegisterMenu />} />
-                <Route path="/confirm-account-deletion/:token" element={<ConfirmAccountDeletion />} />
-                <Route path="/admin/operation-type" element={<AdminOperationType setAlertMessage={setAlertMessage} />} />
-                <Route path="/patient/appointments" element={<PatientAppointments setAlertMessage={setAlertMessage} />} />
-                <Route path="/staff/surgery-rooms" element={<SurgeryRoom setAlertMessage={setAlertMessage} />} />
-                <Route path="/patient/medical-record" element={<PatientMedicalRecord setAlertMessage={setAlertMessage} />} />
+                <Route path="/patient/confirm-account-deletion" element={<ConfirmDeletionPage />} />
+                <Route path="/patient/confirm-profile-update" element={<ConfirmUpdatePage />} />
+
+                {/* Protected Routes */}
+                <Route
+                  path="/admin"
+                  element={
+                    <ProtectedRoute requiredRoles={["Admin"]}>
+                      <AdminPage />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/staff"
+                  element={
+                    <ProtectedRoute requiredRoles={["Doctor", "Nurse", "Technician"]}>
+                      <StaffPage />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/patient"
+                  element={
+                    <ProtectedRoute requiredRoles={["Patient"]}>
+                      <PatientPage />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/admin/patient"
+                  element={
+                    <ProtectedRoute requiredRoles={["Admin"]}>
+                      <AdminPatient setAlertMessage={setAlertMessage} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/admin/staff"
+                  element={
+                    <ProtectedRoute requiredRoles={["Admin"]}>
+                      <AdminStaff setAlertMessage={setAlertMessage} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/staff/operation-requests"
+                  element={
+                    <ProtectedRoute requiredRoles={["Doctor", "Nurse", "Technician"]}>
+                      <StaffOperationRequest setAlertMessage={setAlertMessage} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/admin/operation-type"
+                  element={
+                    <ProtectedRoute requiredRoles={["Admin"]}>
+                      <AdminOperationType setAlertMessage={setAlertMessage} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/patient/appointments"
+                  element={
+                    <ProtectedRoute requiredRoles={["Patient"]}>
+                      <PatientAppointments setAlertMessage={setAlertMessage} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/staff/surgery-rooms"
+                  element={
+                    <ProtectedRoute requiredRoles={["Doctor", "Nurse", "Technician"]}>
+                      <SurgeryRoom setAlertMessage={setAlertMessage} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/patient/medical-record"
+                  element={
+                    <ProtectedRoute requiredRoles={["Patient"]}>
+                      <PatientMedicalRecord setAlertMessage={setAlertMessage} />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/staff/floor" element={<Floor3D2 />} />
+
+                {/* Not Authorized Page */}
+                <Route path="/not-authorized" element={<NotAuthorizedPage />} />
               </Routes>
             </div>
           </main>
